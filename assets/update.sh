b04064bfb19e476cc2b0bf1db65eb342194730d3
wangyongfei <wangyongfei@v-secure.cn>
2020-05-07 09:47:23 +0800

    add updata sql

---
+++ assets/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#echo "source /opt/JyCenter/vhalo/mysql_init/update.sql" |  /opt/JyCenter/local/mysql/bin/mysql -uroot -proot
+
+
+
---
+++ assets/update.sql
@@ -0,0 +1,71 @@
+
+USE vehicle;
+delimiter //
+drop procedure if exists AddColumnUnlessExists;
+create procedure AddColumnUnlessExists(
+    IN dbName tinytext,
+    IN tableName tinytext,
+    IN fieldName tinytext,
+    IN fieldDef text)
+begin
+    IF NOT EXISTS (
+            SELECT * FROM information_schema.COLUMNS
+            WHERE column_name=fieldName
+              and table_name=tableName
+              and table_schema=dbName
+        )
+    THEN
+        set @ddl=CONCAT('ALTER TABLE ',dbName,'.',tableName,
+                        ' ADD COLUMN ',fieldName,' ',fieldDef);
+        prepare stmt from @ddl;
+        execute stmt;
+    END IF;
+end;
+call AddColumnUnlessExists('vehicle', 'managers', 'have_login', 'tinyint(1) default 0');
+//
+
+
+delimiter //
+drop procedure if exists UpdateColumnUnlessExists;
+create procedure UpdateColumnUnlessExists(
+    IN dbName tinytext,
+    IN tableName tinytext,
+    IN fieldName tinytext,
+    IN fieldDef text)
+begin
+    IF EXISTS (
+            SELECT * FROM information_schema.COLUMNS
+            WHERE column_name=fieldName
+              and table_name=tableName
+              and table_schema=dbName
+        )
+    THEN
+        set @ddl=CONCAT('ALTER TABLE ',dbName,'.',tableName,
+                        ' MODIFY ',fieldName,' ',fieldDef);
+        prepare stmt from @ddl;
+        execute stmt;
+    END IF;
+end;
+
+call UpdateColumnUnlessExists('vehicle', 'gw_infos', 'gw_id', 'varchar(255) NULL DEFAULT NULL unique');
+call UpdateColumnUnlessExists('vehicle', 'gw_infos', 'name', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'gw_infos', 'module', 'varchar(255) NULL DEFAULT NULL');
+
+call UpdateColumnUnlessExists('vehicle', 'gw_info_uncerteds', 'gw_id', 'varchar(255) NULL DEFAULT NULL unique');
+call UpdateColumnUnlessExists('vehicle', 'gw_info_uncerteds', 'name', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'gw_info_uncerteds', 'module', 'varchar(255) NULL DEFAULT NULL');
+
+call UpdateColumnUnlessExists('vehicle', 'gw_assets', 'gw_id', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'gw_assets', 'asset_id', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'gw_assets', 'name', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'gw_assets', 'mac', 'varchar(255) NULL DEFAULT NULL');
+
+call UpdateColumnUnlessExists('vehicle', 'threats', 'gw_id', 'varchar(255) NULL DEFAULT NULL');
+call UpdateColumnUnlessExists('vehicle', 'threats', 'gw_asset_id', 'varchar(255) NULL DEFAULT NULL');
+
+call UpdateColumnUnlessExists('vehicle', 'notifies', 'asset_name', 'varchar(255) NULL DEFAULT NULL');
+//
+
+
+
+
