d7261338792f3f4fb0dfc56fc7a76878bb47e02f
wangyongfei <wangyongfei@v-secure.cn>
2020-05-14 19:13:53 +0800

    update shell

--- src/vehicle_script/api_script/flow_api.go
+++ src/vehicle_script/api_script/flow_api.go
@@ -3,14 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 	"vehicle_system/src/vehicle_script/tool"
 )
 
 var urls = map[string]string{
 	"get_flow":   "http://%s:7001/api/v1/flows/%s",
 	"get_flows":  "http://%s:7001/api/v1/flows",
-	"flow_dps":   "http://localhost:7001/api/v1/flow_dps",
-	"pagination": "http://localhost:7001/api/v1/pagination/flows",
+	"flow_dps":   "http://%s:7001/api/v1/flow_dps",
+	"pagination": "http://%s:7001/api/v1/pagination/flows",
 	"post_flows": "http://localhost:7001/api/v1/flows",
 	"edit_flows": "http://localhost:7001/api/v1/flows/1111",
 	"dele_flows": "http://localhost:7001/api/v1/flows/3648327872",
@@ -18,15 +20,17 @@ var urls = map[string]string{
 
 func main() {
 	//getFlow()
-	getFlows()
 	//getPaginationFlows()
+	getflowsDps()
+
+	//unused
+	//getFlows()
 	//addFlows()
 
 	//editFlows()
 	//deleFlows()
 
 	//pushFlow()
-	//getflowsDps()
 }
 
 var flowip string
@@ -39,6 +43,27 @@ func init() {
 	flowip = apiConfigMap["server_ip"]
 	flowvehicleId = apiConfigMap["vehicle_id"]
 	flowId = apiConfigMap["flowid"]
+}
+
+func getflowsDps() {
+	defaultStartTime := GetFewDayAgo(10) //2
+	now := strconv.Itoa(int(time.Now().Unix()))
+
+	token := tool.GetVehicleToken()
+
+	queryParams := map[string]interface{}{
+		"vehicle_id": flowvehicleId,
+		"start_time": defaultStartTime,
+		"end_time":   now,
+	}
+
+	reqUrl := urls["flow_dps"]
+	reqUrl = fmt.Sprintf(reqUrl, flowip)
+
+	resp, _ := tool.Get(reqUrl, queryParams, token)
+
+	respMarshal, _ := json.Marshal(resp)
+	fmt.Printf("resp %+v", string(respMarshal))
 }
 
 func getFlows() {
@@ -59,6 +84,33 @@ func getFlows() {
 	fmt.Printf("resp %+v", string(respMarshal))
 }
 
+func GetFewDayAgo(days int) string {
+	currentTime := time.Now()
+	oldTime := currentTime.AddDate(0, 0, -days).Unix()
+	r := strconv.Itoa(int(oldTime))
+	return r
+}
+
+func getPaginationFlows() {
+	defaultStartTime := GetFewDayAgo(10) //2
+	now := strconv.Itoa(int(time.Now().Unix()))
+
+	token := tool.GetVehicleToken()
+
+	queryParams := map[string]interface{}{
+		"vehicle_id": flowvehicleId,
+		"start_time": defaultStartTime,
+		"end_time":   now,
+	}
+
+	reqUrl := urls["pagination"]
+	reqUrl = fmt.Sprintf(reqUrl, flowip)
+
+	resp, _ := tool.Get(reqUrl, queryParams, token)
+
+	respMarshal, _ := json.Marshal(resp)
+	fmt.Printf("resp %+v", string(respMarshal))
+}
 func getFlow() {
 	token := tool.GetVehicleToken()
 
@@ -68,21 +120,6 @@ func getFlow() {
 
 	reqUrl := urls["get_flow"]
 	reqUrl = fmt.Sprintf(reqUrl, flowip, flowId)
-
-	resp, _ := tool.Get(reqUrl, queryParams, token)
-
-	respMarshal, _ := json.Marshal(resp)
-	fmt.Printf("resp %+v", string(respMarshal))
-}
-
-func getflowsDps() {
-	token := tool.GetVehicleToken()
-
-	queryParams := map[string]interface{}{
-		"vehicle_id": "754d2728b4e549c5a16c0180fcacb800",
-	}
-
-	reqUrl := urls["flow_dps"]
 
 	resp, _ := tool.Get(reqUrl, queryParams, token)
 
@@ -131,23 +168,6 @@ func editFlows() {
 		"dst_ip":     "42",
 	}
 	resp, _ := tool.PutForm(urlReq, bodyParams, token)
-
-	respMarshal, _ := json.Marshal(resp)
-	fmt.Printf("resp %+v", string(respMarshal))
-}
-
-func getPaginationFlows() {
-
-	token := tool.GetVehicleToken()
-
-	queryParams := map[string]interface{}{
-		"vehicle_id": "754d2728b4e549c5a16c0180fcacb800",
-		"page_size":  "2",
-	}
-
-	reqUrl := urls["pagination"]
-
-	resp, _ := tool.Get(reqUrl, queryParams, token)
 
 	respMarshal, _ := json.Marshal(resp)
 	fmt.Printf("resp %+v", string(respMarshal))
