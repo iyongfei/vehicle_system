07fa11140c20047ac261a403b0d227f82b0a1e6a
wangyongfei <wangyongfei@v-secure.cn>
2020-06-01 12:11:12 +0800

    update log code

--- src/vehicle/timing/license_timer.go
+++ src/vehicle/timing/license_timer.go
@@ -1,9 +1,13 @@
 package timing
 
-import "fmt"
+import (
+	"time"
+	"vehicle_system/src/vehicle/logger"
+	"vehicle_system/src/vehicle/util"
+)
 
-func licenseCron()  {
-	fmt.Println("license....")
-
+func licenseCron() {
+	logger.Logger.Print("%s licenseCron:%v", util.RunFuncName(), time.Now())
+	logger.Logger.Info("%s licenseCron:%v", util.RunFuncName(), time.Now())
 
 }
