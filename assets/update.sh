22f7c5f50eb1e67f40dde18324be9dcbae146bf7
wangyongfei <wangyongfei@v-secure.cn>
2020-06-03 18:36:10 +0800

    add update.sh

--- assets/update.sh
+++ assets/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#echo "source /opt/JyCenter/vhalo/mysql_init/update.sql" |  /opt/JyCenter/local/mysql/bin/mysql -uroot -proot
+echo "source /opt/JyCenter/vhalo/mysql_init/update.sql" |  /opt/JyCenter/local/mysql/bin/mysql -uroot -proot
 
 
 
