1ce4a561d326f73d1c1863e8728a53122a296c5c
wangyongfei <wangyongfei@v-secure.cn>
2020-06-10 16:01:17 +0800

    update sql shell

--- assets/update.sql
+++ assets/update.sql
@@ -1,5 +1,62 @@
 
 USE vehicle;
+
+
+
+
+CREATE TABLE IF NOT EXISTS `fprint_info_actives`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+
+  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
+  `os`   varchar(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_fprint_info_actives_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `fprint_info_passives`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+
+  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
+  `dst_port`   int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_fprint_info_passives_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `fprint_passive_infos`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+
+  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
+  `hash` int(11) UNSIGNED NULL DEFAULT NULL,
+  `src_ip` varchar(255) NULL DEFAULT NULL,
+  `src_port` int(11) NULL DEFAULT NULL,
+  `dst_ip` varchar(255) NULL DEFAULT NULL,
+  `dst_port` int(11) NULL DEFAULT NULL,
+
+  `protocol` tinyint(3) UNSIGNED NULL DEFAULT NULL,
+  `flow_info` varchar(255) NULL DEFAULT NULL,
+  `safe_type` tinyint(3) UNSIGNED NULL DEFAULT NULL,
+  `safe_info` varchar(255) NULL DEFAULT NULL,
+  `start_time`  int(11) UNSIGNED NULL DEFAULT NULL,
+  `last_seen_time` int(11)  UNSIGNED NULL DEFAULT NULL,
+  `src_dst_bytes`  BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `dst_src_bytes`  BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `stat`  tinyint(3) UNSIGNED NULL DEFAULT NULL,
+   PRIMARY KEY (`id`) USING BTREE,
+
+  INDEX `idx_fprint_passive_infos_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+
 delimiter //
 drop procedure if exists AddColumnUnlessExists;
 create procedure AddColumnUnlessExists(
@@ -25,9 +82,3 @@ call AddColumnUnlessExists('vehicle', 'assets', 'access_net', 'tinyint(1) UNSIGN
 call AddColumnUnlessExists('vehicle', 'fstrategies', 'name', 'varchar(255)  NULL DEFAULT NULL');
 //
 
-
-
-新增
-fprint_info_active
-fprint_info_passive
-fprint_passive_infos
--- assets/vehicle.sql
+++ assets/vehicle.sql
@@ -692,59 +692,6 @@ CREATE TABLE IF NOT EXISTS `fprint_infos`  (
   INDEX `idx_fprint_infos_deleted_at`(`deleted_at`) USING BTREE
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `fprint_info_actives`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-
-  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
-  `os`   varchar(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_fprint_info_actives_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `fprint_info_passives`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-
-  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
-  `dst_port`   int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_fprint_info_passives_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `fprint_passive_infos`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-
-  `fprint_info_id` varchar(255)  NULL DEFAULT NULL,
-  `hash` int(11) UNSIGNED NULL DEFAULT NULL,
-  `src_ip` varchar(255) NULL DEFAULT NULL,
-  `src_port` int(11) NULL DEFAULT NULL,
-  `dst_ip` varchar(255) NULL DEFAULT NULL,
-  `dst_port` int(11) NULL DEFAULT NULL,
-
-  `protocol` tinyint(3) UNSIGNED NULL DEFAULT NULL,
-  `flow_info` varchar(255) NULL DEFAULT NULL,
-  `safe_type` tinyint(3) UNSIGNED NULL DEFAULT NULL,
-  `safe_info` varchar(255) NULL DEFAULT NULL,
-  `start_time`  int(11) UNSIGNED NULL DEFAULT NULL,
-  `last_seen_time` int(11)  UNSIGNED NULL DEFAULT NULL,
-  `src_dst_bytes`  BIGINT(20) UNSIGNED NULL DEFAULT NULL,
-  `dst_src_bytes`  BIGINT(20) UNSIGNED NULL DEFAULT NULL,
-  `stat`  tinyint(3) UNSIGNED NULL DEFAULT NULL,
-   PRIMARY KEY (`id`) USING BTREE,
-
-  INDEX `idx_fprint_passive_infos_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
-
 
 CREATE TABLE IF NOT EXISTS `white_assets`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
