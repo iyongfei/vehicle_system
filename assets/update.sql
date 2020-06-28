79af1e5485db4e639b6dba55813a26220fc64b15
wangyongfei <wangyongfei@v-secure.cn>
2020-06-29 02:34:22 +0800

    add get method auth

--- assets/update.sql
+++ assets/update.sql
@@ -24,19 +24,4 @@ end;
 call AddColumnUnlessExists('vehicle', 'assets', 'access_net', 'tinyint(1) UNSIGNED NULL DEFAULT NULL');
 //
 
-CREATE TABLE IF NOT EXISTS `vehicle_auths`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-
-  `vehicle_id` varchar(255) NULL DEFAULT NULL,
-  `auth_time`   int(11) UNSIGNED NULL DEFAULT NULL,
-  `auth_days`   int(11) UNSIGNED NULL DEFAULT NULL,
-  `auth_expire`   int(11) UNSIGNED NULL DEFAULT NULL,
-
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_vehicle_auths_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
 
